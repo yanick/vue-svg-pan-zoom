5d14ddbe41e9e05a407dd2df3f8769ac25e185e1
Yanick Champoux <yanick@babyl.dyndns.org>
2018-01-15 18:51:23 -0500

    grooming stories

---
+++ src/single.stories.vue
@@ -0,0 +1,19 @@
+<template>
+    <SvgPanZoom style="width: 500px; height: 500px; border:1px solid black;"
+        :zoomEnabled="true"
+        :controlIconsEnabled="true"
+        :fit="false"
+        :center="true"
+    >
+        <RawTiger />
+    </SvgPanZoom>
+</template>
+
+<script>
+import RawTiger from './RawTiger.vue';
+import SvgPanZoom from './index';
+
+export default {
+    components: {  SvgPanZoom, RawTiger },
+}
+</script>
