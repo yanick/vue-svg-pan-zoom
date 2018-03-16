8966f50cc7ae6a5c1aa1ec08e586baa6295aed37
Yanick Champoux <yanick@babyl.dyndns.org>
2018-03-16 12:24:59 -0400

    pass the svgpanzoom object around

--- src/stories.js
+++ src/stories.js
@@ -6,13 +6,13 @@ import RawTiger from './RawTiger.vue';
 
 import LayerStory      from './layers.stories.vue';
 import ThumbnailStory  from './thumbnail.stories.vue';
-import SingleStory      from './single.stories.vue';
+import SingleStory     from './single.stories.vue';
+import Event           from './event.stories.vue';
 
 const stories = storiesOf('SvgPanZoom', module)
 .addDecorator( withKnobs )
-.add('single inline SVG', 
-    () => SingleStory
-)
+.add('single inline SVG', () => SingleStory)
+.add('event', () => Event )
 .add( 'layers', () => LayerStory )
 .add( 'thumbnail -- needs to be first story loaded to work', () => ThumbnailStory )
 ;
